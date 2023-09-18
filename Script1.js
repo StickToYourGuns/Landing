c4815027d9b731d7d7520b336fba9e82794794ac
Tim <60299355+StickToYourGuns@users.noreply.github.com>
2023-09-18 18:52:14 +0300

    gigaScript added ♂

--- Script1.js
+++ Script1.js
@@ -10,178 +10,203 @@ const arrowLeft = document.querySelector(".arrow");
 const arrowRight = document.querySelector(".arrow_mirror");
 
 
-arrowLeft.addEventListener("click", () => scroll("left"));
-arrowRight.addEventListener("click", () => scroll("right"));
+arrowLeft.addEventListener("click", () => gigaScroll("left"));
+arrowRight.addEventListener("click", () => gigaScroll("right"));
 
 
-console.log(imageArray)
-console.log(circles)
-console.log(titles)
+function gigaScroll(direction) {
+    function elements(element){
+        let activeElement;
+        let nextElement;
+        let prevElement;
 
-function scroll(direction) {
-    let activeElement;
-    let nextElement;
-    let prevElement;
-
-// ПИКЧИ
-
-    imageArray.forEach (function (value, index) {
-        if (value.classList.contains("active")) {
-            activeElement = index;
-            console.log(activeElement)
-            value.classList.remove("active");
+        element.forEach (function (value, index) {
+            if (value.classList.contains("active")) {
+                activeElement = index;
+                console.log(activeElement)
+                value.classList.remove("active");
+            }
+        });
+        if (direction == "right") {
+            nextElement = activeElement + 1;
+            if (nextElement == element.length) {
+                nextElement = 0;
+            }
+            element[nextElement].classList.add("active")
+        } else {
+            prevElement = activeElement - 1;
+            if (prevElement == -1) {
+                prevElement = (element.length - 1);
+            }
+            element[prevElement].classList.add("active")
         }
-    });
-    if (direction == "right") {
-        nextElement = activeElement + 1;
-        if (nextElement == imageArray.length) {
-            nextElement = 0;
-        }
-        imageArray[nextElement].classList.add("active")
-    } else {
-        prevElement = activeElement - 1;
-        if (prevElement == -1) {
-            prevElement = (imageArray.length - 1);
-        }
-        imageArray[prevElement].classList.add("active")
     }
-
-    // КРУГИ
-
-    circles.forEach (function (value, index) {
-        if (value.classList.contains("active")) {
-            activeElement = index;
-            console.log(activeElement)
-            value.classList.remove("active");
-        }
-    });
-    if (direction == "right") {
-        nextElement = activeElement + 1;
-        if (nextElement == circles.length) {
-            nextElement = 0;
-        }
-        circles[nextElement].classList.add("active")
-    } else {
-        prevElement2 = activeElement - 1;
-        if (prevElement == -1) {
-            prevElement = (circles.length - 1);
-        }
-        circles[prevElement].classList.add("active")
-    }
-
-    // ТАЙТЛЫ
-
-    titles.forEach (function (value, index) {
-        if (value.classList.contains("active")) {
-            activeElement = index;
-            console.log(activeElement)
-            value.classList.remove("active");
-        }
-    });
-    if (direction == "right") {
-        nextElement = activeElement + 1;
-        if (nextElement == titles.length) {
-            nextElement = 0;
-        }
-        titles[nextElement].classList.add("active")
-    } else {
-        prevElement2 = activeElement - 1;
-        if (prevElement == -1) {
-            prevElement = (titles.length - 1);
-        }
-        titles[prevElement].classList.add("active")
-    }
-
-    // ТЕКСТЫ бля ну и хуета
-
-    texts1.forEach (function (value, index) {
-        if (value.classList.contains("active")) {
-            activeElement = index;
-            console.log(activeElement)
-            value.classList.remove("active");
-        }
-    });
-    if (direction == "right") {
-        nextElement = activeElement + 1;
-        if (nextElement == texts1.length) {
-            nextElement = 0;
-        }
-        texts1[nextElement].classList.add("active")
-    } else {
-        prevElement2 = activeElement - 1;
-        if (prevElement == -1) {
-            prevElement = (texts1.length - 1);
-        }
-        texts1[prevElement].classList.add("active")
-    }
-
-    texts2.forEach (function (value, index) {
-        if (value.classList.contains("active")) {
-            activeElement = index;
-            console.log(activeElement)
-            value.classList.remove("active");
-        }
-    });
-    if (direction == "right") {
-        nextElement = activeElement + 1;
-        if (nextElement == texts2.length) {
-            nextElement = 0;
-        }
-        texts2[nextElement].classList.add("active")
-    } else {
-        prevElement2 = activeElement - 1;
-        if (prevElement == -1) {
-            prevElement = (texts2.length - 1);
-        }
-        texts2[prevElement].classList.add("active")
-    }
-
-    texts3.forEach (function (value, index) {
-        if (value.classList.contains("active")) {
-            activeElement = index;
-            console.log(activeElement)
-            value.classList.remove("active");
-        }
-    });
-    if (direction == "right") {
-        nextElement = activeElement + 1;
-        if (nextElement == texts3.length) {
-            nextElement = 0;
-        }
-        texts3[nextElement].classList.add("active")
-    } else {
-        prevElement2 = activeElement - 1;
-        if (prevElement == -1) {
-            prevElement = (texts3.length - 1);
-        }
-        texts3[prevElement].classList.add("active")
-    }
-
-    texts4.forEach (function (value, index) {
-        if (value.classList.contains("active")) {
-            activeElement = index;
-            console.log(activeElement)
-            value.classList.remove("active");
-        }
-    });
-    if (direction == "right") {
-        nextElement = activeElement + 1;
-        if (nextElement == texts4.length) {
-            nextElement = 0;
-        }
-        texts4[nextElement].classList.add("active")
-    } else {
-        prevElement2 = activeElement - 1;
-        if (prevElement == -1) {
-            prevElement = (texts4.length - 1);
-        }
-        texts4[prevElement].classList.add("active")
-    }
+    elements(imageArray);
+    elements(circles);
+    elements(titles);
+    elements(texts1);
+    elements(texts2);
+    elements(texts3);
+    elements(texts4);
 }
 
+// function scroll(direction) {
+//     let activeElement;
+//     let nextElement;
+//     let prevElement;
 
+// // ПИКЧИ
 
-// ПЛАН:
+//     imageArray.forEach (function (value, index) {
+//         if (value.classList.contains("active")) {
+//             activeElement = index;
+//             console.log(activeElement)
+//             value.classList.remove("active");
+//         }
+//     });
+//     if (direction == "right") {
+//         nextElement = activeElement + 1;
+//         if (nextElement == imageArray.length) {
+//             nextElement = 0;
+//         }
+//         imageArray[nextElement].classList.add("active")
+//     } else {
+//         prevElement = activeElement - 1;
+//         if (prevElement == -1) {
+//             prevElement = (imageArray.length - 1);
+//         }
+//         imageArray[prevElement].classList.add("active")
+//     }
 
+//     // КРУГИ
 
-// 3. Тексты
+//     circles.forEach (function (value, index) {
+//         if (value.classList.contains("active")) {
+//             activeElement = index;
+//             console.log(activeElement)
+//             value.classList.remove("active");
+//         }
+//     });
+//     if (direction == "right") {
+//         nextElement = activeElement + 1;
+//         if (nextElement == circles.length) {
+//             nextElement = 0;
+//         }
+//         circles[nextElement].classList.add("active")
+//     } else {
+//         prevElement2 = activeElement - 1;
+//         if (prevElement == -1) {
+//             prevElement = (circles.length - 1);
+//         }
+//         circles[prevElement].classList.add("active")
+//     }
+
+//     // ТАЙТЛЫ
+
+//     titles.forEach (function (value, index) {
+//         if (value.classList.contains("active")) {
+//             activeElement = index;
+//             console.log(activeElement)
+//             value.classList.remove("active");
+//         }
+//     });
+//     if (direction == "right") {
+//         nextElement = activeElement + 1;
+//         if (nextElement == titles.length) {
+//             nextElement = 0;
+//         }
+//         titles[nextElement].classList.add("active")
+//     } else {
+//         prevElement2 = activeElement - 1;
+//         if (prevElement == -1) {
+//             prevElement = (titles.length - 1);
+//         }
+//         titles[prevElement].classList.add("active")
+//     }
+
+//     // ТЕКСТЫ бля ну и хуета
+
+//     texts1.forEach (function (value, index) {
+//         if (value.classList.contains("active")) {
+//             activeElement = index;
+//             console.log(activeElement)
+//             value.classList.remove("active");
+//         }
+//     });
+//     if (direction == "right") {
+//         nextElement = activeElement + 1;
+//         if (nextElement == texts1.length) {
+//             nextElement = 0;
+//         }
+//         texts1[nextElement].classList.add("active")
+//     } else {
+//         prevElement2 = activeElement - 1;
+//         if (prevElement == -1) {
+//             prevElement = (texts1.length - 1);
+//         }
+//         texts1[prevElement].classList.add("active")
+//     }
+
+//     texts2.forEach (function (value, index) {
+//         if (value.classList.contains("active")) {
+//             activeElement = index;
+//             console.log(activeElement)
+//             value.classList.remove("active");
+//         }
+//     });
+//     if (direction == "right") {
+//         nextElement = activeElement + 1;
+//         if (nextElement == texts2.length) {
+//             nextElement = 0;
+//         }
+//         texts2[nextElement].classList.add("active")
+//     } else {
+//         prevElement2 = activeElement - 1;
+//         if (prevElement == -1) {
+//             prevElement = (texts2.length - 1);
+//         }
+//         texts2[prevElement].classList.add("active")
+//     }
+
+//     texts3.forEach (function (value, index) {
+//         if (value.classList.contains("active")) {
+//             activeElement = index;
+//             console.log(activeElement)
+//             value.classList.remove("active");
+//         }
+//     });
+//     if (direction == "right") {
+//         nextElement = activeElement + 1;
+//         if (nextElement == texts3.length) {
+//             nextElement = 0;
+//         }
+//         texts3[nextElement].classList.add("active")
+//     } else {
+//         prevElement2 = activeElement - 1;
+//         if (prevElement == -1) {
+//             prevElement = (texts3.length - 1);
+//         }
+//         texts3[prevElement].classList.add("active")
+//     }
+
+//     texts4.forEach (function (value, index) {
+//         if (value.classList.contains("active")) {
+//             activeElement = index;
+//             console.log(activeElement)
+//             value.classList.remove("active");
+//         }
+//     });
+//     if (direction == "right") {
+//         nextElement = activeElement + 1;
+//         if (nextElement == texts4.length) {
+//             nextElement = 0;
+//         }
+//         texts4[nextElement].classList.add("active")
+//     } else {
+//         prevElement2 = activeElement - 1;
+//         if (prevElement == -1) {
+//             prevElement = (texts4.length - 1);
+//         }
+//         texts4[prevElement].classList.add("active")
+//     }
+// }
