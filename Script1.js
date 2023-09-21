1380eea4c7a19cd5877fc54b15b3020b4d93a9a9
Tim <60299355+StickToYourGuns@users.noreply.github.com>
2023-09-21 18:07:05 +0300

    Minor changes

--- Script1.js
+++ Script1.js
@@ -8,73 +8,68 @@ let texts3 = Array.from(document.querySelectorAll(".specs_discription3"));
 let texts4 = Array.from(document.querySelectorAll(".specs_discription4"));
 
 
-function addingCircles(count) {
-    const parent1 = document.querySelector(".second_step_nav_container");
+function addingSMTH(count) {
+    const circleParent = document.querySelector(".second_step_nav_container");
+    const titleParent = document.querySelector(".second_step_objects");
     for (let i = 1; i <= count; i++) {
         let newCircle = document.createElement("div");
+        let newTitle = document.createElement("p");
         newCircle.classList.add("second_step_nav_item");
         newCircle.classList.add("circle");
-        newCircle.setAttribute("data-number", (i));
-        parent1.append(newCircle);
-        console.log(i)
-    }
-}
-
-addingCircles(addCircles);
-
-function addingTitles(count) {
-    const parent2 = document.querySelector(".second_step_objects");
-    for (let i = 1; i <= count; i++) {
-        let newTitle = document.createElement("p");
         newTitle.classList.add("second_step_object_name");
+        newCircle.setAttribute("data-number", (i));
         newTitle.setAttribute("data-number", (i));
-        parent2.append(newTitle);
+        circleParent.append(newCircle);
+        titleParent.append(newTitle);
         newTitle.innerHTML = (imageArray[i].alt);
     }
+
 }
 
-addingTitles(addCircles);
+addingSMTH(addCircles);
+
 
 let circles = Array.from(document.querySelectorAll(".circle"));
 let titles = Array.from(document.querySelectorAll(".second_step_object_name"));
 
-function gigaScroll(direction) {
-    function handleArrowClick(element) {
-        let activeElement;
-        let nextElement;
-        let prevElement;
 
-        element.forEach(function (value, index) {
-            if (value.classList.contains("active")) {
-                activeElement = index;
-                console.log(activeElement)
-                value.classList.remove("active");
-            }
-        });
-        if (direction == "right") {
-            nextElement = activeElement + 1;
-            if (nextElement == element.length) {
-                nextElement = 0;
-            }
-            element[nextElement].classList.add("active")
-        } else {
-            prevElement = activeElement - 1;
-            if (prevElement == -1) {
-                prevElement = (element.length - 1);
-            }
-            element[prevElement].classList.add("active")
+function scrollClassesSelect(element, direction) {
+    let activeElement;
+    let nextElement;
+    let prevElement;
+
+    element.forEach(function (value, index) {
+        if (value.classList.contains("active")) {
+            activeElement = index;
+            value.classList.remove("active");
         }
+    });
+    if (direction == "right") {
+        nextElement = activeElement + 1;
+        if (nextElement == element.length) {
+            nextElement = 0;
+        }
+        element[nextElement].classList.add("active")
+    } else {
+        prevElement = activeElement - 1;
+        if (prevElement == -1) {
+            prevElement = (element.length - 1);
+        }
+        element[prevElement].classList.add("active")
     }
-    handleArrowClick(imageArray);
-    handleArrowClick(circles);
-    handleArrowClick(titles);
-    handleArrowClick(texts1);
-    handleArrowClick(texts2);
-    handleArrowClick(texts3);
-    handleArrowClick(texts4);
 }
 
-function toggleClasses(element, event) {
+function scrollByElement(direction) {
+    scrollClassesSelect(imageArray, direction);
+    scrollClassesSelect(circles, direction);
+    scrollClassesSelect(titles, direction);
+    scrollClassesSelect(texts1, direction);
+    scrollClassesSelect(texts2, direction);
+    scrollClassesSelect(texts3, direction);
+    scrollClassesSelect(texts4, direction);
+}
+
+function toggleClassesSelect(element, event) {
     element.forEach(function (item) {
         item.classList.remove("active");
     })
@@ -82,30 +77,30 @@ function toggleClasses(element, event) {
 }
 
 function selectByElement(event) {
-    toggleClasses(imageArray, event);
-    toggleClasses(circles, event);
-    toggleClasses(titles, event);
+    toggleClassesSelect(imageArray, event);
+    toggleClassesSelect(circles, event);
+    toggleClassesSelect(titles, event);
 }
 
 
-arrowLeft.addEventListener("click", () => gigaScroll("left"));
-arrowRight.addEventListener("click", () => gigaScroll("right"));
+arrowLeft.addEventListener("click", () => scrollByElement("left"));
+arrowRight.addEventListener("click", () => scrollByElement("right"));
 
 circles.forEach(function (circle) {
-    circle.addEventListener("click", (e) => selectByElement(e));
+    circle.addEventListener("click", (event) => selectByElement(event));
 });
 
 titles.forEach(function (title) {
-    title.addEventListener("click", (e) => selectByElement(e));
+    title.addEventListener("click", (event) => selectByElement(event));
 });
 
 
 // Шаг 1. Окончание слайдера
 // Переназвать переменные (я вроде понял что имеет в виду Соня)
-// Переписать addingCircles и addingTitles и вынести по максимум повторяющиеся строки в одну функцию
+// + Переписать addingCircles и addingTitles и вынести по максимум повторяющиеся строки в одну функцию
 // Вынести handleArrowClick как в тоглклассес, не забыть при этом параметры
 
-// Шаг 2. 
+// Шаг 2.
 // Подсветка якорей
 
 // Шаг 3.
