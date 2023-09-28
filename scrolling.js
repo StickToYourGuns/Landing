c14db7d5c242a9ac44e11e3ae97399e16a74a7cc
Tim <60299355+StickToYourGuns@users.noreply.github.com>
2023-09-28 23:28:04 +0300

    scrolling version1 almost done

--- scrolling.js
+++ scrolling.js
@@ -3,73 +3,84 @@ const menuArray = Array.from(document.querySelectorAll(".menu_item_link"));
 
 // 2
 
-let sectionsArray = [];
+// let sectionsArray = [];
 
 // const IdArray = menuArray.forEach( function (element) {
 //     let id = element.href.split("#")[1];
 //     sectionsArray.push(document.getElementById(id));
 // });
 
+let sectionPositions = [];
 
-function findingSections(array) {
+function findingSections() {
+    let sectionsArray = [];
     menuArray.forEach( function (element) {
         let id = element.href.split("#")[1];
-        array.push(document.getElementById(id));
+        sectionsArray.push(document.getElementById(id));
     })
+    findSectionPositions(sectionsArray);
 }
 
-findingSections(sectionsArray);
+findingSections()
+
 
 
 console.log(menuArray);
-console.log(sectionsArray);
 
+function findSectionPositions(array) {
+    sectionPositions = array.map(function (section){
+        return (section.getBoundingClientRect().top);
+    })
+    console.log(sectionPositions);
+    return (sectionPositions);
+}
 
-// 3
+// если позиция скролла > sectionPositions[0], то мы делаем активным menuArray[0], если sectionPositions[1], то мы делаем активным menuArray[1] и тд
 
-
-const rect = sectionsArray[1].getBoundingClientRect();
-const xPosition = rect.left;
-const yPosition = rect.top;
-console.log(xPosition);
-console.log(yPosition);
-
-
-
-
-
-// Позиция скролла
+function comparePositions (scrollPosition) {
+    if ((scrollPosition >= sectionPositions[0]) && (scrollPosition < sectionPositions[1])) {
+        console.log(scrollPosition, sectionPositions[0]);
+        menuArray.forEach (function (element) {
+            element.classList.remove("active");
+        })
+        menuArray[0].classList.add("active");
+    } else if ((scrollPosition >= sectionPositions[1]) && (scrollPosition < sectionPositions[2])) {
+        console.log(scrollPosition, sectionPositions[1]);
+        menuArray.forEach (function (element) {
+            element.classList.remove("active");
+        })
+        menuArray[1].classList.add("active");
+    } else if ((scrollPosition >= sectionPositions[2]) && (scrollPosition < sectionPositions[3])) {
+        console.log(scrollPosition, sectionPositions[2]);
+        menuArray.forEach (function (element) {
+            element.classList.remove("active");
+        })
+        menuArray[2].classList.add("active");
+    } else if ((scrollPosition >= sectionPositions[3]) && (scrollPosition < sectionPositions[4])) {
+        console.log(scrollPosition, sectionPositions[3]);
+        menuArray.forEach (function (element) {
+            element.classList.remove("active");
+        })
+        menuArray[3].classList.add("active");
+    } else if ((scrollPosition >= sectionPositions[4]) && (scrollPosition < sectionPositions[5])) {
+        console.log(scrollPosition, sectionPositions[3]);
+        menuArray.forEach (function (element) {
+            element.classList.remove("active");
+        })
+        menuArray[4].classList.add("active");
+    }
+}
 
 
 window.addEventListener('scroll', function () {
     const scrollPosition = window.scrollY;
-    console.log(scrollPosition);
+    comparePositions (scrollPosition)
 });
 
 
 
-// function scrolling(scrollPosition) {
-//     if (scrollPosition > 0 && scrollPosition <= id2) {
-//         делаем активным первый 
-//     }
-// }
 
-// function scrolling(scrollPosition, sectionsArray) {
-//     if (scrollPosition > sectionsArray[0] && scrollPosition <= sectionsArray[1]) {
-//         menuItemLink[0] = active; 
-//     }
-// }
+// план:
 
-//   позиция элемента
-
-// const element = document.getElementById('myDiv');
-// const rect = element.getBoundingClientRect();
-// const xPosition = rect.left;
-// const yPosition = rect.top;
-
-// План
-// + 1находим элементы меню. Делаем массив
-// + 2находим названия нужных нам id и ищем по ним html секции. Делаем из них массив
-// Нужно найти позиции этих секций. то есть из предыдущего массива добыть id
-// 3затем нам нужно сделать функцию со следующим принципом работы:
-// 4сопоставляем позицию скролла с расположениями элементов. Мы работаем с массивами, поэтому если позиция скролла больше, чем позиция первого элемента из массива секций
+// 1.Переписать компейр
+// 2.Заказать пепперони
