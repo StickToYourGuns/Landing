0bdc569f089383a718cd605ece338e3da6662f01
Tim <60299355+StickToYourGuns@users.noreply.github.com>
2023-10-06 18:01:15 +0300

    video controls added

--- video.js
+++ video.js
@@ -1,34 +1,41 @@
 const video = document.querySelector(".video");
-const videoButton = document.querySelector(".video_button");
+const videoButtonPlay = document.querySelector(".video_button_play");
+const videoButtonPause = document.querySelector(".video_button_pause");
 const control = [];
-control.push(video, videoButton);
+control.push(videoButtonPlay, videoButtonPause);
 console.log(video);
-console.log(videoButton);
+console.log(videoButtonPlay);
 console.log(control);
 
-// function playVideo(media) {
-//     media.play();
-// }
-
-// function pauseVideo(media) {
-//     media.pause();
-// }
+const bs = document.querySelector(".blackscreen");
 
 control.forEach(function(element) {
     element.addEventListener("click", function(e) {
         video.classList.toggle("pause")
-        videoButton.classList.toggle("pause");
+        videoButtonPlay.classList.toggle("pause");
         if (video.paused) {
             video.play();
+            bs.classList.remove("hidden")
         } else {
             video.pause();
+            bs.classList.add("hidden")
         }
     })
+})
+
+control.forEach(function(element) {
     element.addEventListener("mouseover", function(e) {
-        videoButton.classList.add("onmouse");
+        videoButtonPause.classList.remove("pause")
+        videoButtonPause.style.scale = 1.1;
+        videoButtonPlay.style.scale = 1.1;
     })
+})
+
+control.forEach(function(element) {
     element.addEventListener("mouseout", function(e) {
-        videoButton.classList.remove("onmouse");
+        videoButtonPause.classList.add("pause")
+        videoButtonPause.style.scale = 1;
+        videoButtonPlay.style.scale = 1;
     })
 })
 
